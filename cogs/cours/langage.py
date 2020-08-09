c225031e799093ba4e1bf8a9f2fd501112a0d25d
theo-hubgrade <theo.ferreira@epitech.eu>
2020-08-09 02:51:22 +0200

    Indented the if / elif statement...

--- cogs/cours/langage.py
+++ cogs/cours/langage.py
@@ -12,13 +12,13 @@ class Langage(commands.Cog):
         lg = arg
         if lg == "java":
             embed = discord.Embed(
-            title = 'Apprendre de Java',
-            colour = discord.Colour.red()
-            )
-        embed.set_author(name='MxCommunity',icon_url='https://mxcommunity.xyz/src/MxCommunity_tr.png')
-        embed.add_field(name='Pourquoi apprendre le Java', value='• Visual Studio Code (multi-langage)\n• Sublime Text (multi-langage)\n• pyCharm (Python)\n• Eclipse (Java)\n• IntelliJ (Java)\n• Android Studio (Java & Kotlin)\n• Aptana Studio 3 (HTML - CSS  - JS)\n• PhpStorm (PHP)\n• WebStorm (JavaScript)\n• RubyMine (Ruby)', inline=True)
-        embed.add_field(name='Ou apprendre le Java', value='• GitHub\n• GitLab\n• Repl.it', inline=True)
-        await ctx.send(embed=embed)
+                title = 'Apprendre de Java',
+                colour = discord.Colour.red()
+                )
+            embed.set_author(name='MxCommunity',icon_url='https://mxcommunity.xyz/src/MxCommunity_tr.png')
+            embed.add_field(name='Pourquoi apprendre le Java', value='• Visual Studio Code (multi-langage)\n• Sublime Text (multi-langage)\n• pyCharm (Python)\n• Eclipse (Java)\n• IntelliJ (Java)\n• Android Studio (Java & Kotlin)\n• Aptana Studio 3 (HTML - CSS  - JS)\n• PhpStorm (PHP)\n• WebStorm (JavaScript)\n• RubyMine (Ruby)', inline=True)
+            embed.add_field(name='Ou apprendre le Java', value='• GitHub\n• GitLab\n• Repl.it', inline=True)
+            await ctx.send(embed=embed)
         elif lg == "pyhton":
             print("2")
         else:
