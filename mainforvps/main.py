6d9a766c29035abdb9f153403604342c1bdeefdf
MxLily <44614832+Mx-Plume@users.noreply.github.com>
2020-08-09 03:09:55 +0200

    Create main.py

---
+++ mainforvps/main.py
@@ -0,0 +1,33 @@
+import discord
+import os
+import dotenv
+from discord.utils import get
+from discord.ext import commands
+from dotenv import load_dotenv
+
+print("Start...")
+
+bot = discord.Client()
+bot = commands.Bot(command_prefix='&')
+load_dotenv()
+
+@bot.event
+async def on_ready():
+    print("Lily's Secretary :  ON")
+    print("Version : 1.0.0")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Secrétaire Officiel De Lily"))
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+jeton = os.getenv('DISCORD_TOKEN')
+bot.run(jeton)
